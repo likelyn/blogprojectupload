b3b06bdae07447afee17a54b703b9fdfed62e7c5
hyerin <hexriin@gmail.com>
2019-07-31 01:13:40 +0900

    first commit

--- fourthfourth/fourthproject/settings.py
+++ fourthfourth/fourthproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
